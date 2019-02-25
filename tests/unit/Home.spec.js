7f0756fee750f21f55d855b36f6559dc4a857389
Roberto Miguez <robertmgz@yahoo.co.uk>
2019-02-25 23:09:23 +0000

    unit tests

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,20 +1,20 @@
 import { shallowMount } from '@vue/test-utils'
-import Home from '@/views/Home'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import Home from '@/views/Home'
 
 Vue.use(Vuetify)
 
 describe('Home', () => {
   it('renders the component', () => {
     // arrange
-    const wrapper = shallowMount(Home, {
-      mocks: {
-        $vuetify: { breakpoint: {} }
-      }
-    })
-
+    const wrapper = shallowMount(Home)
     // assert
     expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('Checking DIV subtitle tag text', () =>{
+    const wrapper = shallowMount(Home)
+    const div = wrapper.find('div')
+    expect(div.text()).toBe('Bemvindo ao Consultório!')
   })
 })
